68fe6d7204e921ba1829a1da2f2edd5f34475611
Wout Goesaert <goesaert@zuidplas.strw.leidenuniv.nl>
2022-03-23 11:22:50 +0100

    Grote commit vanaf Linux, geen idee wat er veranderd is exact.

--- Fluxgetter_BDSF.py
+++ Fluxgetter_BDSF.py
@@ -15,14 +15,24 @@ from astropy import units as u
 
 import bdsf
 
+'''WITH FREQ AVERAGE:'''
 directory_univ_mkt = r'/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/'
-img = bdsf.process_image(directory_univ_mkt+'Abell_85_Linpol_Freqmean.fits')
-img.export_image('Abell85_ch0_BDSF_output.fits', img_type='ch0')
-img.export_image('Abell85_rms_BDSF_output.fits', img_type='rms')
-img.export_image('Abell85_mean_BDSF_output.fits', img_type='mean')
-img.export_image('Abell85_gaus_model_BDSF_output.fits', img_type='gaus_model')
-img.export_image('Abell85_gaus_resid_BDSF_output.fits', img_type='gaus_resid')
+img = bdsf.process_image('Abell_85_Linpol_Freqmean.fits', rms_box=(30, 10), ini_gausfit='simple', peak_maxsize=10, trim_box=(1000,2000,1000,2000), use_scipy_fft=True, output_all = True)
+img.write_catalog(outfile=r'Abell85_catalog_BDSF_output_freqmean_rmsbox.fits', catalog_type='gaul', format='ds9', clobber='True')
 
-img.write_catalog('Abell85_catalog_BDSF_output.fits', catalog_type='gaul')
+img.export_image(outfile=r'Abell85_ch0_BDSF_output_freqmean_rmsbox.fits', img_type='ch0', clobber='True')
+img.export_image(outfile=r'Abell85_rms_BDSF_output_freqmean_rmsbox.fits', img_type='rms', clobber='True')
+img.export_image(outfile=r'Abell85_mean_BDSF_output_freqmean_rmsbox.fits', img_type='mean', clobber='True')
+img.export_image(outfile=r'Abell85_gaus_model_BDSF_output_freqmean_rmsbox.fits', img_type='gaus_model', clobber='True')
+img.export_image(outfile=r'Abell85_gaus_resid_BDSF_output_freqmean_rmsbox.fits', img_type='gaus_resid', clobber='True')
 
+'''WITH MULTIPLE FREQS:'''
+directory_univ_mkt = r'/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/'
+img_2 = bdsf.process_image('Abell_85_Linpol.fits', rms_box=(30, 10), ini_gausfit='simple', peak_maxsize=10, trim_box=(1000,2000,1000,2000), use_scipy_fft=True, output_all = True)
+img_2.write_catalog(outfile=r'Abell85_catalog_BDSF_output_rmsbox.fits', catalog_type='gaul', format='ds9', clobber='True')
 
+img_2.export_image(outfile=r'Abell85_ch0_BDSF_output_rmsbox.fits', img_type='ch0', clobber='True')
+img_2.export_image(outfile=r'Abell85_rms_BDSF_output_rmsbox.fits', img_type='rms', clobber='True')
+img_2.export_image(outfile=r'Abell85_mean_BDSF_output_rmsbox.fits', img_type='mean', clobber='True')
+img_2.export_image(outfile=r'Abell85_gaus_model_BDSF_output_rmsbox.fits', img_type='gaus_model', clobber='True')
+img_2.export_image(outfile=r'Abell85_gaus_resid_BDSF_output_rmsbox.fits', img_type='gaus_resid', clobber='True')
