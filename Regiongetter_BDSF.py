2eb0d4343141c3eb81f8d6214eb8c6f15afeeaba
Wout Goesaert <goesaert@zuidplas.strw.leidenuniv.nl>
2022-03-29 17:48:35 +0200

    PyBDSF tuning

--- Regiongetter_BDSF.py
+++ Regiongetter_BDSF.py
@@ -16,23 +16,23 @@ from astropy import units as u
 import bdsf
 
 '''WITH FREQ AVERAGE:'''
-directory_univ_mkt = r'/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/'
-img = bdsf.process_image('Abell_85_Linpol_Freqmean.fits', rms_box=(30, 10), ini_gausfit='simple', peak_maxsize=10, trim_box=(1000,2000,1000,2000), use_scipy_fft=True, output_all = True)
-img.write_catalog(outfile=r'Abell85_catalog_BDSF_output_freqmean_rmsbox.fits', catalog_type='gaul', format='ds9', clobber='True')
+#directory_univ_mkt = r'/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/'
+#img = bdsf.process_image('Abell_85_Linpol_Freqmean.fits', rms_box=(30, 10), trim_box=(0, 1000,0,1000), use_scipy_fft=True, output_all = True)
+#img.write_catalog(outfile=r'Abell85_catalog_BDSF_output_freqmean_rmsbox.reg', catalog_type='gaul', format='ds9', clobber='True')
 
-img.export_image(outfile=r'Abell85_ch0_BDSF_output_freqmean_rmsbox.fits', img_type='ch0', clobber='True')
-img.export_image(outfile=r'Abell85_rms_BDSF_output_freqmean_rmsbox.fits', img_type='rms', clobber='True')
-img.export_image(outfile=r'Abell85_mean_BDSF_output_freqmean_rmsbox.fits', img_type='mean', clobber='True')
-img.export_image(outfile=r'Abell85_gaus_model_BDSF_output_freqmean_rmsbox.fits', img_type='gaus_model', clobber='True')
-img.export_image(outfile=r'Abell85_gaus_resid_BDSF_output_freqmean_rmsbox.fits', img_type='gaus_resid', clobber='True')
+#img.export_image(outfile=r'Abell85_ch0_BDSF_freqmean.fits', img_type='ch0', clobber='True')
+#img.export_image(outfile=r'Abell85_rms_BDSF_freqmean.fits', img_type='rms', clobber='True')
+#img.export_image(outfile=r'Abell85_mean_BDSF_freqmean.fits', img_type='mean', clobber='True')
+#img.export_image(outfile=r'Abell85_gaus_model_BDSF_freqmean.fits', img_type='gaus_model', clobber='True')
+#img.export_image(outfile=r'Abell85_gaus_resid_BDSF_freqmean.fits', img_type='gaus_resid', clobber='True')
 
 '''WITH MULTIPLE FREQS:'''
 directory_univ_mkt = r'/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/'
-img_2 = bdsf.process_image('Abell_85_Linpol.fits', rms_box=(30, 10), ini_gausfit='simple', peak_maxsize=10, trim_box=(1000,2000,1000,2000), use_scipy_fft=True, output_all = True)
-img_2.write_catalog(outfile=r'Abell85_catalog_BDSF_output_rmsbox.fits', catalog_type='gaul', format='ds9', clobber='True')
+img_2 = bdsf.process_image('Abell_85_aFix_pol_I_Farcsec_fcube_cor.fits', rms_box=(30, 10), use_scipy_fft=True, output_all = True)
+img_2.write_catalog(outfile=r'Abell85_catalog_BDSF_output_rmsbox.reg', catalog_type='gaul', format='ds9', clobber='True')
 
-img_2.export_image(outfile=r'Abell85_ch0_BDSF_output_rmsbox.fits', img_type='ch0', clobber='True')
-img_2.export_image(outfile=r'Abell85_rms_BDSF_output_rmsbox.fits', img_type='rms', clobber='True')
-img_2.export_image(outfile=r'Abell85_mean_BDSF_output_rmsbox.fits', img_type='mean', clobber='True')
-img_2.export_image(outfile=r'Abell85_gaus_model_BDSF_output_rmsbox.fits', img_type='gaus_model', clobber='True')
-img_2.export_image(outfile=r'Abell85_gaus_resid_BDSF_output_rmsbox.fits', img_type='gaus_resid', clobber='True')
+img_2.export_image(outfile=r'Abell85_ch0_BDSF.fits', img_type='ch0', clobber='True')
+img_2.export_image(outfile=r'Abell85_rms_BDSF.fits', img_type='rms', clobber='True')
+img_2.export_image(outfile=r'Abell85_mean_BDSF.fits', img_type='mean', clobber='True')
+img_2.export_image(outfile=r'Abell85_gaus_model_BDSF.fits', img_type='gaus_model', clobber='True')
+img_2.export_image(outfile=r'Abell85_gaus_resid_BDSF.fits', img_type='gaus_resid', clobber='True')
